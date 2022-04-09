abb5303d88d5da37918fc01d01243eb1986f3d7a
piyushlohar2205 <83692075+piyushlohar2205@users.noreply.github.com>
2022-04-09 17:45:30 +0530

    .

--- BooksSystem.py
+++ BooksSystem.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-con = sqlite3.connect("bookmngsys.db",check_same_thread=False)
+con = sqlite3.connect("bookslibrary.db", check_same_thread=False)
 
 listOfTables1 = con.execute("SELECT name from sqlite_master WHERE type='table' AND name='BOOKS' ").fetchall()
 
